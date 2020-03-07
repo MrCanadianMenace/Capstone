86563dcecd4f145810b64dbd4fdcaf27e6530e7c
Patrick Egan <MrCanadianMenace@gmail.com>
2020-03-07 01:24:43 -0500

    Added 32 bit fixed point support to simulation

--- hardware/butterfly_sum/Vsim/fixed_point.h
+++ hardware/butterfly_sum/Vsim/fixed_point.h
@@ -1,11 +1,14 @@
 #pragma once
 #include <cnl/fixed_point.h>
 
+// Create a fixed point type which has 20 integer bits and 12 fractional bits
+typedef cnl::fixed_point<cnl::uint16, -12> fixpoint_32;
 // Create a fixed point type which has 10 integer bits and 6 fractional bits
 typedef cnl::fixed_point<cnl::uint16, -6> fixpoint_16;
 // Create a fixed point type which has 5 integer bits and 3 fractional bits
 typedef cnl::fixed_point<cnl::uint16, -3> fixpoint_8;
 
 // Short conversion functions to switch primitive integer types back to fixed point
-fixpoint_8 int_to_fixed8(unsigned char);
+fixpoint_8 int_to_fixed8(signed char);
 fixpoint_16 int_to_fixed16(short signed int);
+fixpoint_32 int_to_fixed32(long signed int);
