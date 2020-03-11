6845ec2315a0d6ab66b5fe2df0a0ed3a19fc0c20
Patrick Egan <pegan3@uwo.ca>
2020-03-11 18:59:12 -0400

    Updated verilator simulation files to support multiplier changes

--- hardware/butterfly_sum/Vsim/VTest.cpp
+++ hardware/butterfly_sum/Vsim/VTest.cpp
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <iostream>
 
-#include "Vbutterfly_sum.h"
+#include "Vbutterfly_sum_helper.h"
 #include "verilated.h"
 
 #include "testbench.hpp"
@@ -12,64 +12,79 @@ int main(int argc, char **argv) {
     // Initialize Verliators variables
     Verilated::commandArgs(argc, argv);
     // Create an instance of our module under test
-    TESTBENCH<Vbutterfly_sum> *tb = new TESTBENCH<Vbutterfly_sum>();
+    TESTBENCH<Vbutterfly_sum_helper> *tb = new TESTBENCH<Vbutterfly_sum_helper>();
     // Open trace file for waveform viewing
     tb->opentrace("trace.vcd");
 
     /* First Test Case */
-    Complex inputA = Complex(fixpoint_16{1}, fixpoint_16{2});
-    Complex inputB = Complex(fixpoint_16{3}, fixpoint_16{4});
-    Complex inTwiddle = Complex(fixpoint_16{1}, fixpoint_16{0});
+    Complex inputA = Complex(fixpoint_37{1}, fixpoint_37{2});
+    Complex inputB = Complex(fixpoint_37{3}, fixpoint_37{4});
+    Complex inTwiddle = Complex(fixpoint_37{1}, fixpoint_37{0});
 
-    Complex outA = Complex(fixpoint_16{0}, fixpoint_16{0});
-    Complex outB = Complex(fixpoint_16{0}, fixpoint_16{0});
+    Complex outA = Complex(fixpoint_37{0}, fixpoint_37{0});
+    Complex outB = Complex(fixpoint_37{0}, fixpoint_37{0});
 
     // Begin test inputs
-    tb->m_core->i_A = inputA.to_input();
-    tb->m_core->i_B = inputB.to_input();
-    tb->m_core->i_twiddle = inTwiddle.to_input();
+    tb->m_core->i_A_real = inputA.real_input();
+    tb->m_core->i_A_imag = inputA.imag_input();
+    tb->m_core->i_B_real = inputB.real_input();
+    tb->m_core->i_B_imag = inputB.imag_input();
+    tb->m_core->i_twiddle_real = inTwiddle.real_input();
+    tb->m_core->i_twiddle_imag = inTwiddle.imag_input();
     tb->tick();
 
-    outA.from_output(tb->m_core->o_A);
-    outB.from_output(tb->m_core->o_B);
+    outA.from_output(tb->m_core->o_A_real, tb->m_core->o_A_imag);
+    outB.from_output(tb->m_core->o_B_real, tb->m_core->o_B_imag);
 
-    std::cout << "Output: A=" << outA.real << "+(" << outA.imag << "i) B=" 
-            << outB.real << "+(" << outB.imag << "i)" << std::endl;
+    std::cout << "Input: A= " << inputA.real << " + (" << inputA.imag << "i)"  
+                << ", B= " << inputB.real << " + (" << inputB.imag << "i)" << std::endl;
+    std::cout << "Output: A=" << outA.real << " + (" << outA.imag << "i) B=" 
+            << outB.real << " + (" << outB.imag << "i)" << std::endl;
 
     
     /* Second Test Case */
-    inputA = Complex(fixpoint_16{1.25}, fixpoint_16{2.5});
-    inputB = Complex(fixpoint_16{3.5}, fixpoint_16{4.25});
-    inTwiddle = Complex(fixpoint_16{1}, fixpoint_16{0});
+    inputA = Complex(fixpoint_37{1.25}, fixpoint_37{2.5});
+    inputB = Complex(fixpoint_37{3.5}, fixpoint_37{4.25});
+    inTwiddle = Complex(fixpoint_37{1}, fixpoint_37{0});
 
     // Begin test inputs
-    tb->m_core->i_A = inputA.to_input();
-    tb->m_core->i_B = inputB.to_input();
-    tb->m_core->i_twiddle = inTwiddle.to_input();
+    tb->m_core->i_A_real = inputA.real_input();
+    tb->m_core->i_A_imag = inputA.imag_input();
+    tb->m_core->i_B_real = inputB.real_input();
+    tb->m_core->i_B_imag = inputB.imag_input();
+    tb->m_core->i_twiddle_real = inTwiddle.real_input();
+    tb->m_core->i_twiddle_imag = inTwiddle.imag_input();
     tb->tick();
 
-    outA.from_output(tb->m_core->o_A);
-    outB.from_output(tb->m_core->o_B);
+    outA.from_output(tb->m_core->o_A_real, tb->m_core->o_A_imag);
+    outB.from_output(tb->m_core->o_B_real, tb->m_core->o_B_imag);
 
+    std::cout << "Input: A= " << inputA.real << "+ (" << inputA.imag << "i)"  
+                << ", B= " << inputB.real << "+ (" << inputB.imag << "i)" << std::endl;
     std::cout << "Output: A=" << outA.real << "+(" << outA.imag << "i) B=" 
             << outB.real << "+(" << outB.imag << "i)" << std::endl;
 
     /* Second Test Case */
-    inputA = Complex(fixpoint_16{-1.25}, fixpoint_16{2.5});
-    inputB = Complex(fixpoint_16{3.5}, fixpoint_16{-4.25});
-    inTwiddle = Complex(fixpoint_16{1}, fixpoint_16{0});
+    inputA = Complex(fixpoint_37{-1.25}, fixpoint_37{2.5});
+    inputB = Complex(fixpoint_37{3.5}, fixpoint_37{-4.25});
+    inTwiddle = Complex(fixpoint_37{1}, fixpoint_37{0});
 
     // Begin test inputs
-    tb->m_core->i_A = inputA.to_input();
-    tb->m_core->i_B = inputB.to_input();
-    tb->m_core->i_twiddle = inTwiddle.to_input();
+    tb->m_core->i_A_real = inputA.real_input();
+    tb->m_core->i_A_imag = inputA.imag_input();
+    tb->m_core->i_B_real = inputB.real_input();
+    tb->m_core->i_B_imag = inputB.imag_input();
+    tb->m_core->i_twiddle_real = inTwiddle.real_input();
+    tb->m_core->i_twiddle_imag = inTwiddle.imag_input();
     tb->tick();
 
-    outA.from_output(tb->m_core->o_A);
-    outB.from_output(tb->m_core->o_B);
+    outA.from_output(tb->m_core->o_A_real, tb->m_core->o_A_imag);
+    outB.from_output(tb->m_core->o_B_real, tb->m_core->o_B_imag);
 
-    std::cout << "Output: A=" << outA.real << "+(" << outA.imag << "i) B=" 
-            << outB.real << "+(" << outB.imag << "i)" << std::endl;
+    std::cout << "Input: A= " << inputA.real << " + (" << inputA.imag << "i)"  
+                << ", B= " << inputB.real << " + (" << inputB.imag << "i)" << std::endl;
+    std::cout << "Output: A=" << outA.real << " + (" << outA.imag << "i) B=" 
+            << outB.real << " + (" << outB.imag << "i)" << std::endl;
 
     // Cleanup before exiting
     tb->closetrace();
--- hardware/butterfly_sum/Vsim/fixed_point.h
+++ hardware/butterfly_sum/Vsim/fixed_point.h
@@ -1,29 +1,30 @@
 #pragma once
 #include <cnl/fixed_point.h>
 
-// Create a fixed point type which has 20 integer bits and 12 fractional bits
-typedef cnl::fixed_point<cnl::int32, -12> fixpoint_32;
-// Create a fixed point type which has 10 integer bits and 6 fractional bits
+// Create a fixed point type which has 18 integer bits and 18 fractional bits
+typedef cnl::fixed_point<cnl::int64, -18> fixpoint_37;
+
+// Create a fixed point type which has 9 integer bits and 6 fractional bits
 typedef cnl::fixed_point<cnl::int16, -6> fixpoint_16;
-// Create a fixed point type which has 5 integer bits and 3 fractional bits
+// Create a fixed point type which has 4 integer bits and 3 fractional bits
 typedef cnl::fixed_point<cnl::int8, -3> fixpoint_8;
 
 
 // Short conversion functions to switch primitive integer types back to fixed point
 fixpoint_8 int_to_fixed8(signed char);
 fixpoint_16 int_to_fixed16(short signed int);
-fixpoint_32 int_to_fixed32(long signed int);
+fixpoint_37 int_to_fixed37(long long int);
 
 class Complex {
     public:
         // Members
-        fixpoint_16 real;
-        fixpoint_16 imag;
-
+        fixpoint_37 real;
+        fixpoint_37 imag;
 
         // Methods
-        Complex(fixpoint_16, fixpoint_16);
+        Complex(fixpoint_37, fixpoint_37);
 
-        long signed int to_input();
-        void from_output(long signed int out);
+        long long int real_input();
+        long long int imag_input();
+        void from_output(long long int real_int, long long int imag_int);
 };
