a91a1686ae9e518d31cde1d4a9364200e1b32877
Patrick Egan <pegan3@uwo.ca>
2020-03-27 01:12:17 -0400

    Reorganized and cleaned stuff up

--- hardware/Pipeline/QBuildnFlash.sh
+++ hardware/Pipeline/QBuildnFlash.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/sh
 
-quartus_map --read_settings_files=on --write_settings_files=off FFT_Pipeline -c FFT_Pipeline
-quartus_fit --read_settings_files=off --write_settings_files=off FFT_Pipeline -c FFT_Pipeline
-quartus_asm --read_settings_files=off --write_settings_files=off FFT_Pipeline -c FFT_Pipeline
-quartus_sta FFT_Pipeline -c FFT_Pipeline
+# Move into directory with Quartus files
+pushd QuartusProject
+
+# Quartus compilation steps broken into individual commands
+quartus_map --read_settings_files=on --write_settings_files=off FFT_Pipeline -c FFT_Pipeline && \
+quartus_fit --read_settings_files=off --write_settings_files=off FFT_Pipeline -c FFT_Pipeline && \
+quartus_asm --read_settings_files=off --write_settings_files=off FFT_Pipeline -c FFT_Pipeline && \
+quartus_sta FFT_Pipeline -c FFT_Pipeline && \
+
+# Program connected DE2-115 board with the compilation output
 quartus_pgm -c "USB-Blaster [1-4]" -m jtag -o 'P;FFT_Pipeline.sof'
+
+# Return to script directory
+popd
